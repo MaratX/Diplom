9780dfaf13e9ba95d28db64ecae53068162531d6
HMF <marioX16@mail.ru>
2017-02-22 11:55:53 +0300

    0.2.1 редирект доработан добавлен классы Account, JDBC JDBC - прописан connector, statement and close Account - доработать

--- src/lib_dep/Account.java
+++ src/lib_dep/Account.java
@@ -1,7 +1,7 @@
 package lib_dep;
 
-import java.io.IOException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by HMF on 21.02.2017.
@@ -9,14 +9,19 @@ import java.sql.ResultSet;
 public class Account {
     private JDBC jdbc = new JDBC();
     private ResultSet rs;
-    private String SqlAuthentication = "select idUser, pass from user";
+    private String SqlAuthentication = "DECLARE @return BIT = 0;" +
+            "IF EXISTS (SELECT userId, userPass FROM USER " +
+            "WHERE userId = ? and userPass = ?) SET @return = 1;";
 
 
     public boolean Authentication (String login, String password){
 
         try{
+            rs = jdbc.getStmt().executeQuery(SqlAuthentication);
+            while (rs.next()){
 
-        }catch (IOException e){
+            }
+        }catch (SQLException e){
 
         }
         return false;
